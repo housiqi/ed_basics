1991a280fab836fc6bdeb3ef78a495b71b2a31c6
Siqi Hou <48788834+housiqi@users.noreply.github.com>
2020-06-23 17:15:39 +0800

    Create testwriting.py

---
+++ testwriting.py
@@ -0,0 +1,13 @@
+# print(52//2)
+# print(10%2)
+
+# a=[2,4,1,5,23,2,41]
+# d=enumerate(a)
+# print(d[0])
+# print('****')
+# print(d[1])
+# print(d)
+# index=a.index(23)
+# print(index)
+
+import scipy as sp
